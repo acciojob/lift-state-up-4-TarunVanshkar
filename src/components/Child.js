669b614f71154393e8017b4f525e5c1777163424
TarunVanshkar <tarunben33@gmail.com>
2023-08-02 22:43:58 +0530

    submitting code-Wed Aug 02 2023 22:43:56 GMT+0530 (India Standard Time)

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,35 +1,24 @@
+import React,{useState} from "react";
 
-import React from "react";
-
-const Child = ({itemList, setItemList}) => {
-    const handleRemove = (id) => {
-        let i = 0;
-        let tempItemList = [];
-        itemList.map((item, index) => {
-            if(index !== id){
-                tempItemList.push(item)
-            }
-            i++;
-        })
-        setItemList(tempItemList)
+const Child=({cartItems,onRemoveItem})=>{
+    const handleRemoveClick=(itemId)=>{
+        onRemoveItem(itemId);
     }
-  return (
-    <div className="child">
-        <h2>Child Component</h2>
-        
-        <ul>
-            {
-                itemList.map((item, index) => {
-                    return <li key={index}>
-                        <span id="itemName">{item.itemName} - </span>
-                        <span id="itemPrice">${item.itemPrice}</span>
-                        <button onClick={() => handleRemove(index)}>Remove</button>
+
+    return(
+        <div className="child">
+            <h2>Child Component</h2>
+            <ul>
+                {cartItems && cartItems.map((item)=>(
+                    <li key={item.id}>
+                        <span id="itemName">{item.name}</span>
+                        <span id="itemPrice">{` - $${item.price}`}</span>
+                        <button onClick={()=>handleRemoveClick(item.id)}>Remove</button>
                     </li>
-                })
-            }
-        </ul>
-    </div>
-  )
+                ))}
+            </ul>
+        </div>
+    )
 }
 
-export default Child
+export default Child;
