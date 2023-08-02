b1bc8ff35315ff981c8e19f8a16edddaed8e4dc7
TarunVanshkar <tarunben33@gmail.com>
2023-08-02 22:36:01 +0530

    submitting code-Wed Aug 02 2023 22:35:56 GMT+0530 (India Standard Time)

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,35 +1,60 @@
-
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Child from "./Child";
 
 const Parent = () => {
-    const [cartItem, setCartItem] = useState([]);
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState('');
+    const [itemName, setItemName] = useState('');
+    const [itemPrice, setItemPrice] = useState('');
+    const [cartItems, setCartItems] = useState([
+        { id: 1, name: 'Item 1', price: 10 },
+        { id: 2, name: 'Item 2', price: 20 },
+        { id: 3, name: 'Item 3', price: 30 }
+    ]);
 
-    let handleSubmit = () => {
-        console.log("clicked", name, price)
-        setCartItem([...cartItem, {
-            itemName : name,
-            itemPrice : price
-        }])
-        console.log(cartItem)
+
+    const handleAddItem=()=>{
+        let newItem;
+        if(itemName&&itemPrice){
+            newItem={
+                id: cartItems.length+1,
+                name: itemName,
+                price: Number(itemPrice),
+            }
+        }
+        setCartItems((prevItems)=>[...prevItems,newItem]);
+        setItemName('');
+        setItemPrice('');
+    }
+
+    const handleRemoveItem = (itemId) => {
+
+        setCartItems((prevItems) => {
+            console.log("this is previtems", prevItems);
+            return prevItems.filter((item) => item.id !== itemId)
+
+        })
     }
 
     return (
-    <div className="parent">
-        <h1>Parent Component</h1>
-        <label htmlFor="itemName">Item Name:</label>
-        <input type="text" id="itemName" onChange={(e) => setName(e.target.value)} value={name} />
+        <div className="parent">
 
-        <label htmlFor="itemPrice">Item Price:</label>
-        <input type="number" id="itemPrice" onChange={(e) => setPrice(e.target.value)} value={price} />
-        
-        <button onClick={() => handleSubmit()}>Add Item</button>
+            <h1>Parent Component</h1>
+            <div className="inputs">
+                <input id="itemName"
+                    type="text" value={itemName} onChange={(e) => setItemName(e.target.value)} placeholder="Item Name" />
+                <input id="itemPrice"
+                    type="number"
+                    value={itemPrice}
+                    onChange={(e) => setItemPrice(e.target.value)}
+                    placeholder="Item Price"
+                />
+                <button onClick={handleAddItem}>Add Item</button>
+            </div>
+            <Child cartItems={cartItems} onRemoveItem={handleRemoveItem}></Child>
 
-        <Child itemList={cartItem} setItemList={setCartItem} />
-    </div>
-  )
+
+        </div>
+    )
+
 }
 
-export default Parent
+export default Parent;
